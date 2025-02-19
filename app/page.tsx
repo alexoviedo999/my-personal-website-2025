97c77c41c2e98bc417ea0f66b99c3a22016aa3ea
Alejandro Oviedo <alexoviedo999@gmail.com>
2025-02-18 22:48:55 -0600

    adding perlin noise to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,14 @@
 'use client'
 
+import getLayer from '@/components/canvas/getLayer'
 import dynamic from 'next/dynamic'
 import Link from 'next/link'
 import { Suspense } from 'react'
+import { OrbitControls } from '@react-three/drei'
+import { ViewLoader } from '@/components/canvas/ViewLoader'
 
-const Logo = dynamic(() => import('@/components/canvas/Examples').then((mod) => mod.Logo), { ssr: false })
-
-const Dog = dynamic(() => import('@/components/canvas/Examples').then((mod) => mod.Dog), { ssr: false })
-
-const Duck = dynamic(() => import('@/components/canvas/Examples').then((mod) => mod.Duck), { ssr: false })
-
-const Paradox = dynamic(() => import('@/components/canvas/Examples').then((mod) => mod.Paradox), { ssr: false })
-
-const View = dynamic(() => import('@/components/canvas/View').then((mod) => mod.View), {
+const NoiseGrid = dynamic(() => import('@/components/canvas/NoiseGrid').then((mod) => mod.NoiseGrid), {
   ssr: false,
-  loading: () => (
-    <div className='flex h-96 w-full flex-col items-center justify-center'>
-      <svg className='-ml-1 mr-3 size-5 animate-spin text-black' fill='none' viewBox='0 0 24 24'>
-        <circle className='opacity-25' cx='12' cy='12' r='10' stroke='currentColor' strokeWidth='4' />
-        <path
-          className='opacity-75'
-          fill='currentColor'
-          d='M4 12a8 8 0 0 1 8-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 0 1 4 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z'
-        />
-      </svg>
-    </div>
-  ),
 })
 
 const Common = dynamic(() => import('@/components/canvas/View').then((mod) => mod.Common), { ssr: false })
@@ -35,12 +18,13 @@ export default function Page() {
     <>
       <div className='relative min-h-screen'>
         <div className='absolute inset-0'>
-          <View className='h-screen w-full'>
+          <ViewLoader className='h-screen w-full'>
             <Suspense fallback={null}>
-              <Logo route='/blob' scale={0.6} position={[0, 0, 0]} />
-              <Common color={'lightblue'} />
+              <NoiseGrid />
+              <OrbitControls />
+              <Common color={undefined} />
             </Suspense>
-          </View>
+          </ViewLoader>
         </div>
         <div className='relative z-10 mx-auto flex w-full flex-col flex-wrap items-center md:flex-row'>
           <div className='hero min-h-[70vh]'>
---
+++ src/components/canvas/NoiseGrid.tsx
@@ -0,0 +1,56 @@
+'use client'
+
+import { useFrame, useLoader } from '@react-three/fiber'
+import * as THREE from 'three'
+import { ImprovedNoise } from '@/utils/ImprovedNoise.js'
+import React from 'react'
+const Noise = new ImprovedNoise()
+
+type PointsRef = THREE.Points & {
+  geometry: THREE.BufferGeometry
+}
+
+export const NoiseGrid = ({}) => {
+  const ref = React.useRef<PointsRef>(null)
+  const planeGeo = new THREE.PlaneGeometry(6, 6, 48, 48)
+  const coords = planeGeo.attributes.position
+  let colors = []
+  let col = new THREE.Color()
+  const p = new THREE.Vector3()
+  const nScale = 0.75
+  const zPosScale = 1.0
+  const lowColor = new THREE.Color(0.0, 0.3, 0.6)
+  const highColor = new THREE.Color(1.0, 1.0, 1.0)
+  let lightnessMult = 3.0
+  let elapsedTime = 0
+
+  useFrame((_, t) => {
+    elapsedTime += t * 0.5
+    const geo = ref.current.geometry
+    const verts = geo.attributes.position
+    let ns
+    colors = []
+    for (let i = 0; i < coords.count; i += 1) {
+      p.fromBufferAttribute(verts, i)
+      ns = Noise.noise(p.x * nScale, p.y * nScale, elapsedTime)
+      p.z = ns * zPosScale
+      verts.setXYZ(i, p.x, p.y, p.z)
+      col.lerpColors(lowColor, highColor, ns * lightnessMult)
+      let { r, g, b } = col
+      colors.push(r, g, b)
+    }
+    geo.setAttribute('position', verts)
+    geo.setAttribute('color', new THREE.Float32BufferAttribute(colors, 3))
+    verts.needsUpdate = true
+  })
+
+  const sprite = useLoader(THREE.TextureLoader, './circle.png')
+  return (
+    <points ref={ref}>
+      <bufferGeometry>
+        <bufferAttribute attach={'attributes-position'} count={coords.count} array={coords.array} itemSize={3} />
+      </bufferGeometry>
+      <pointsMaterial alphaTest={0.5} vertexColors size={0.1} map={sprite} />
+    </points>
+  )
+}
